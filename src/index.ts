57df7035387c8ed1038a92d57e23a89a86e0d6c4
Ivan Baena <baenadev.io@gmail.com>
2020-08-11 00:23:57 -0400

    create generic collection class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'newer name', age: 0 });
-
-console.log(user.get('name'));
-
-user.on('save', () => {
-  console.log(user);
+const collection = User.buildUserCollection();
+collection.on('change', () => {
+  console.log(collection);
 });
-
-user.save();
+collection.fetch();
